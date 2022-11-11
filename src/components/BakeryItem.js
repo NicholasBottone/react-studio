1d6165716bbcec80b8b94b3dc725ff1ad3ed8dcd
Nicholas Bottone <nick@bottone.io>
2022-11-11 02:51:00 -0500

    Add BakeryItem component

--- src/components/BakeryItem.js
+++ src/components/BakeryItem.js
@@ -1,1 +1,27 @@
-// TODO: create a component that displays a single bakery item
+import React from "react";
+import { Button, Card } from "react-bootstrap";
+
+/*
+  item: {
+      name: string;
+      description: string;
+      price: number;
+      image: string;
+  };
+  addToCart: () => void;
+*/
+export default function BakeryItem({ item, addToCart }) {
+  return (
+    <Card style={{ height: "100%" }}>
+      <Card.Img variant="top" src={item.image} />
+      <Card.Body>
+        <Card.Title>{item.name}</Card.Title>
+        <Card.Text>{item.description}</Card.Text>
+        <Card.Text>${item.price}</Card.Text>
+        <Button variant="primary" onClick={addToCart}>
+          Add to Cart
+        </Button>
+      </Card.Body>
+    </Card>
+  );
+}
